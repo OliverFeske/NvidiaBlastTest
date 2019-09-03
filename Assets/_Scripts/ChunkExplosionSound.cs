1051a54091c1d92f863a2156e30e02f5922682ba
Nackenmassage <oliverfeske@hotmail.com>
2019-09-03 14:48:14 +0200

    Second Commit / Explosion Finished :)

---
+++ Assets/_Scripts/ChunkExplosionSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkExplosionSound : MonoBehaviour
+{
+    private ExplosionTest explosionTest;
+
+    void Start()
+    {
+        explosionTest = GameObject.Find("ExplosionPoint").GetComponent<ExplosionTest>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        explosionTest.PlaySound();
+    }
+}
--- Assets/_Scripts/ExplosionTest.cs
+++ Assets/_Scripts/ExplosionTest.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class ExplosionTest : MonoBehaviour
 {
+    [Header("Objects")]
+    [SerializeField] private AudioClip explosionSound;
+    [SerializeField] private GameObject explosionEffect;
     [SerializeField] private Transform explosionPos;
+    [Header("Variables")]
     [SerializeField] private float explosionStrength = 20f;
     [SerializeField] private float explosionRadius = 2f;
     [SerializeField] private float upwardsMod = 3f;
+    [SerializeField] private float explosionTimer = 7f;
+
+    private AudioSource source;
     private Destructible[] destructibles;
-    [SerializeField] private Rigidbody[] rbs;
+    private Rigidbody[] rbs;
+    private bool canExplode = true;
 
     void Start()
     {
         destructibles = GetComponentsInChildren<Destructible>();
+        source = GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && canExplode)
         {
-            for (int i = 0; i < destructibles.Length; i++)
-            {
-                destructibles[i].GetForce();
-            }
+            destructibles = GetComponentsInChildren<Destructible>();
+            StartCoroutine(DoExplosion());
         }
     }
 
@@ -36,10 +43,32 @@ public class ExplosionTest : MonoBehaviour
         }
     }
 
+    public void PlaySound()
+    {
+        source.PlayOneShot(explosionSound);
+    }
+
     void OnDrawGizmos()
     {
         if (explosionPos == null) { return; }
         Gizmos.DrawWireSphere(explosionPos.position, explosionRadius);
         Gizmos.color = Color.red;
+    }
+
+    IEnumerator DoExplosion()
+    {
+        for (int i = 0; i < destructibles.Length; i++)
+        {
+            destructibles[i].GetForce();
+        }
+
+        explosionEffect.SetActive(true);
+        source.PlayOneShot(explosionSound);
+        canExplode = false;
+
+        yield return new WaitForSeconds(7f);
+
+        explosionEffect.SetActive(false);
+        canExplode = true;
     }
 }
