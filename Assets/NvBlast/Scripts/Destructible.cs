714887f462b244644eeda6dfc0580b63f704af6c
Nackenmassage <oliverfeske@hotmail.com>
2019-09-03 13:06:50 +0200

    First Commit / Standard Assets Included

---
+++ Assets/NvBlast/Scripts/Destructible.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour {
+
+    public GameObject fracturedPrefab;
+    private ExplosionTest explosionTest;
+
+    void Start()
+    {
+        explosionTest = transform.parent.GetComponent<ExplosionTest>();
+    }
+
+    //    private void OnMouseDown()
+    //    {
+    //        Instantiate(fracturedPrefab, transform.position, transform.rotation);
+    //        Destroy(transform.root.gameObject);
+    //    }
+
+    public void GetForce()
+    {
+        GameObject chunkPrefab = (GameObject)Instantiate(fracturedPrefab, transform.position, transform.rotation);
+        explosionTest.AddExplosionForceToObject(chunkPrefab);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/_Scripts/ExplosionTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionTest : MonoBehaviour
+{
+    [SerializeField] private Transform explosionPos;
+    [SerializeField] private float explosionStrength = 20f;
+    [SerializeField] private float explosionRadius = 2f;
+    [SerializeField] private float upwardsMod = 3f;
+    private Destructible[] destructibles;
+    [SerializeField] private Rigidbody[] rbs;
+
+    void Start()
+    {
+        destructibles = GetComponentsInChildren<Destructible>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            for (int i = 0; i < destructibles.Length; i++)
+            {
+                destructibles[i].GetForce();
+            }
+        }
+    }
+
+    public void AddExplosionForceToObject(GameObject _chunkPrefab)
+    {
+        rbs = _chunkPrefab.GetComponentsInChildren<Rigidbody>();
+        for (int i = 0; i < rbs.Length; i++)
+        {
+            rbs[i].AddExplosionForce(explosionStrength, explosionPos.position, explosionRadius, upwardsMod);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (explosionPos == null) { return; }
+        Gizmos.DrawWireSphere(explosionPos.position, explosionRadius);
+        Gizmos.color = Color.red;
+    }
+}
